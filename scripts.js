4c876376f57836a1a182f93402e6b829e3736c0f
boikanyobaitshepi <156679487+boikanyobaitshepi@users.noreply.github.com>
2024-05-16 09:57:45 +0200

    Register the BookPreview custom element

--- scripts.js
+++ scripts.js
@@ -1,13 +1,57 @@
-class BookPreview extends HTMLElement{
-  constructor(){
-    super()
-  } 
+// Define the BookPreview web component
+class BookPreview extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    const { author, id, image, title } = this.dataset;
+    this.render(author, id, image, title);
+  }
+  import { books, authors, genres, BOOKS_PER_PAGE } from "./data.js";
+  render(author, id, image, title) {
+    this.shadowRoot.innerHTML = `
+      <style>
+        .preview {
+          border: none;
+          background: none;
+          cursor: pointer;
+          display: flex;
+          align-items: center;
+        }
+        .preview__image {
+          width: 50px;
+          height: 75px;
+          object-fit: cover;
+          margin-right: 10px;
+        }
+        .preview__info {
+          display: flex;
+          flex-direction: column;
+        }
+        .preview__title {
+          font-size: 1em;
+          margin: 0;
+        }
+        .preview__author {
+          font-size: 0.875em;
+          color: grey;
+        }
+      </style>
+      <button class="preview" data-preview="${id}">
+        <img class="preview__image" src="${image}" alt="Book cover" />
+        <div class="preview__info">
+          <h3 class="preview__title">${title}</h3>
+          <div class="preview__author">${author}</div>
+        </div>
+      </button>
+    `;
+  }
 }
-connectedCallback()
 
-const now = new Date();
-customElements.define('Book-preview', BookPreview);
-
+// Register the BookPreview custom element
+customElements.define('book-preview', BookPreview);
 
 
 
